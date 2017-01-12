c416cdb4065da44ab4e74f90a50be707e7109b56
fuzzybear3965 <johnrichardrinehart@gmail.com>
2017-01-12 18:33:52 -0500

    Make the first paragraph turn red on click.

--- my_interactivity.js
+++ my_interactivity.js
@@ -1,7 +1,13 @@
 var pretty_texts = document.getElementsByClassName("pretty-text");
-console.log(pretty_texts[0]);
 
 for (var i = 0; i < pretty_texts.length; i++) {
    var pretty_text = pretty_texts[i];
    pretty_text.style.color = "blue";
+   if (i == 0) {
+      pretty_text.addEventListener("click", turnMeRed);
+   }
+}
+
+function turnMeRed() {
+   this.style.color = "red";
 }
