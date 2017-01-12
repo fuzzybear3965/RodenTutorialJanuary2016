c0af5432e20195eda22bdc20b24707b7511258c2
fuzzybear3965 <johnrichardrinehart@gmail.com>
2017-01-12 17:52:42 -0500

    Added a loop to change all of the pretty-text elements to blue.

--- my_interactivity.js
+++ my_interactivity.js
@@ -1,2 +1,7 @@
 var pretty_texts = document.getElementsByClassName("pretty-text");
 console.log(pretty_texts[0]);
+
+for (var i = 0; i < pretty_texts.length; i++) {
+   var pretty_text = pretty_texts[i];
+   pretty_text.style.color = "blue";
+}
